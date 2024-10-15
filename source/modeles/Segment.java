3411c794d2571526055d7a9a2452d1f30b93bb7c
adrienhdx <42543223+adrienhdx@users.noreply.github.com>
2024-10-15 16:35:50 +0200

    quelques modeles + JUnit

---
+++ source/modeles/Segment.java
@@ -0,0 +1,48 @@
+package source.modeles;
+
+public class Segment {
+
+    private Noeud origine;
+    private Noeud destination;
+    private double longueur;
+    private String nomRue;
+
+    public Segment(String nomRue, Noeud origine, Noeud destination, double longueur) {
+        this.nomRue = nomRue;
+        this.origine = origine;
+        this.destination = destination;
+        this.longueur = longueur;
+    }
+
+    public Noeud getOrigine() {
+        return origine;
+    }
+
+    public void setOrigine(Noeud origine) {
+        this.origine = origine;
+    }
+
+    public Noeud getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Noeud destination) {
+        this.destination = destination;
+    }
+
+    public double getLongueur() {
+        return longueur;
+    }
+
+    public void setLongueur(double longueur) {
+        this.longueur = longueur;
+    }
+
+    public String getNomRue() {
+        return nomRue;
+    }
+
+    public void setNomRue(String nomRue) {
+        this.nomRue = nomRue;
+    }
+}
---
+++ source/modeles/Ville.java
@@ -0,0 +1,15 @@
+package source.modeles;
+
+import java.util.List;
+
+public class Ville {
+
+    private int num_noeuds;
+    private int num_segments;
+    private List<Noeud> noeuds;
+    private List<Segment> segments;
+
+    public double CalculerDistance(Noeud depart, Noeud arrivee) {
+        return 0;
+    }
+}
