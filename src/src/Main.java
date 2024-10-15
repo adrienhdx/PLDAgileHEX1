6438c6edf120d9fad43b41407014019921ae5237
AUB <roche.aubin@gmail.com>
2024-10-15 14:57:45 +0200

    [NEW] classe test extraction XML

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,10 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException {
         System.out.println("Hello world!");
+        Test_xml.traiterXml(new String[]{});
     }
 }
