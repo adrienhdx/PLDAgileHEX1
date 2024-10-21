e3e2909ca00f8bb183820bcff7da78a3b1853db5
José Pablo Southerland <jose.southerland@usm.cl>
2024-10-21 10:54:11 +0200

    changement de nom d'extractVertices()

--- source/main.java
+++ source/main.java
@@ -8,19 +8,13 @@ import source.model.Vertex;
 
 public class main {
     public static void main(String[] args) {
-        Map<String, Vertex> map = XmlExtractor.extractPlan("./resources/grandPlan.xml");
+        Map<String, Vertex> map = XmlExtractor.extractPlan("./resources/moyenPlan.xml");
         List<Delivery> demandes = XmlExtractor.extractDemande("./resources/demandeMoyen5.xml", map);
-        System.out.println("Hello world!");
         for (Delivery demande : demandes) {
             System.out.println(demande);
         }
         List<Vertex> demandesVertex = XmlExtractor.extractDeliveryVertices(demandes);
         for (Vertex vertex : demandesVertex) {
-            System.out.println(vertex);
-        }
-
-        List<Vertex> vertices = XmlExtractor.extractVertices(map, demandesVertex);
-        for (Vertex vertex : vertices) {
             System.out.println(vertex);
         }
     }
